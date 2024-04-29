3c3cf520185e2cb4ee9f51a34ab1d791953b1e9b
Yusuf <yusufknsz@hotmail.com>
2024-04-29 14:10:03 +0300

    Python

---
+++ AsalSayıYazmayanKod.py
@@ -0,0 +1,16 @@
+﻿n = int(input("Bir sayı giriniz"))
+sayı = 2
+while sayı <= n:
+    sayaç = 0
+    bolen = 1
+    while bolen <= sayı:
+        if sayı % bolen == 0:
+            bolen = bolen + 1
+            sayaç = sayaç + 1
+        else:
+            bolen = bolen + 1
+    if sayaç == 2:
+        pass
+    else:
+        print(sayı)
+    sayı = sayı + 1
---
+++ TauSayısıHesap.py
@@ -0,0 +1,13 @@
+﻿sayı = int(input("Bir sayı giriniz"))
+sayaç = 0
+bolen = 1
+while bolen <= sayı:
+    if sayı % bolen == 0:
+        bolen = bolen + 1
+        sayaç = sayaç + 1
+    else:
+        bolen = bolen + 1
+if sayı % sayaç == 0:
+    print("tau sayısıdır")
+else:
+    print("tau sayısı değildir")
